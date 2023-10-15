f54728f552bb14db8ef5e5735648cc3cfbac4666
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-10-15 02:02:06 -0300

    Simulando um CRUD - Iniciado

---
+++ api_restful/src/main/java/com/curso_spring/api_restful/repositories/ProdutoRepositorio.java
@@ -0,0 +1,78 @@
+package com.curso_spring.api_restful.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.curso_spring.api_restful.model.Produto;
+
+@Repository
+public class ProdutoRepositorio {
+
+    private List<Produto> produtos = new ArrayList<>();
+    private Integer ultimoId = 0;
+
+    /**
+     * Método que retorna uma lista com todos os produtos cadastrados
+     * @return produtos
+     */
+    public List<Produto> obterTodos() {
+        return produtos;
+    }
+
+    /**
+     * Método que retorna um produto com base no seu id
+     * @param id do produto
+     * @return Produto da base de dados
+     */
+    public /* Optional<Produto> */ Produto obterPorId(Integer id) {
+        // minha solução
+        Produto produtoValidacao = new Produto();
+        produtoValidacao.setId(id);
+        return produtos.get(produtos.indexOf(produtoValidacao));
+
+        // solução da aula
+        // return produtos.stream().filter((Produto p) -> p.getId() == id).findFirst().get();
+    }
+
+    /**
+     * Recebe um produto e cadastra na lista da base de dados
+     * @param produto que será adicionado
+     * @return produto adicionado
+     */
+    public Produto adicionar(Produto produto) {
+        produto.setId(++ultimoId);
+        produtos.add(produto);
+        return produto;
+    }
+
+    /**
+     * Atualiza um determinado produto de acordo com as novas informações fornecidas pelo usuário
+     * @param produto
+     * @return
+     */
+    public Produto atualizar(Produto produto) {
+        produtos.set(produtos.indexOf(produto), produto);
+        return produto;
+        // método do professor -> encontrar produto por id, deletar e depois salvar novamente com o mesmo id mas com
+        // as novas informações
+    }
+
+    /**
+     * Método para deletar um produto baseado no id informado
+     * @param id informado
+     * @return void
+     */
+    public Void deletar(Integer id) {
+        // minha solução
+        Produto produtoValidacao = new Produto();
+        produtoValidacao.setId(id);
+        produtos.remove(produtoValidacao);
+        return null;
+
+        // solução professor
+        // produtos.removeIf(x -> x.getId() == id);
+        // return null;
+    }
+}
---
+++ api_restful/src/test/java/com/curso_spring/api_restful/ApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_spring.api_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
